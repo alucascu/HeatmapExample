5d9b6da1cab109624fdaf02cbe0e137e8898a519
alucascu <159496888+alucascu@users.noreply.github.com>
2024-02-12 21:51:07 -0500

    Add files via upload

---
+++ HeatmapHeat.m
@@ -0,0 +1,26 @@
+clear all
+close all
+syms x  y;
+
+syms k real
+
+a = 0;
+b = 10;
+
+T = (4 / ((2*k + 1) * pi * sinh(pi*(2*k + 1)))) * sin((2*k + 1)*pi*x) * sinh((2*k + 1)*pi*y);
+
+tiledlayout(2, 2)               W
+    nexttile
+    s = symsum(T, k ,a,b);
+    fsurf(s, [0 1 0 1])
+b=100;
+    nexttile
+    s = symsum(T, k ,a,b);
+    fsurf(s, [0 1 0 1])
+
+
+b = 1000;
+    nexttile([1,2])
+    s = symsum(T, k ,a,b);
+    fsurf(s, [0 1 0 1])
+
