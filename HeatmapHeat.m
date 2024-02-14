4f2d6dc212ddb8ee7c3e744fe773c9341cc34f55
alucascu <159496888+alucascu@users.noreply.github.com>
2024-02-13 21:33:00 -0500

    Update HeatmapHeat.m
    removed a "w"

--- HeatmapHeat.m
+++ HeatmapHeat.m
@@ -9,7 +9,7 @@ b = 10;
 
 T = (4 / ((2*k + 1) * pi * sinh(pi*(2*k + 1)))) * sin((2*k + 1)*pi*x) * sinh((2*k + 1)*pi*y);
 
-tiledlayout(2, 2)               W
+tiledlayout(2, 2)
     nexttile
     s = symsum(T, k ,a,b);
     fsurf(s, [0 1 0 1])
